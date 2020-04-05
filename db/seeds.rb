53ea1127e853f63b7a7661291c7eae8659ae66de
isabel k. lee <hello@isabelklee.co>
2020-04-05 13:35:57 -0400

    create migration, seed data, controller, class, index route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "Avocado", store: "Trader Joe's")
+Coupon.create(coupon_code: "Banana", store: "Trader Joe's")
+Coupon.create(coupon_code: "Chickpeas", store: "Whole Foods")
+Coupon.create(coupon_code: "Figs", store: "Whole Foods")
